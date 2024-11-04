e5cb7f697f84b669a967cc0ff754e2fa39eae1b3
Matan-Shimon <63747865+Matan-Shimon@users.noreply.github.com>
2024-11-04 10:05:27 +0200

    Delete dbConnect.php

--- dbConnect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	// connect to DB
-	$conn = mysqli_connect('localhost', 'matan', '1234', "inmanage_db");
-
-	// check connection
-	if(!$conn) {
-		echo "Connection error: ".mysqli_connect_error();
-	}
-?>
